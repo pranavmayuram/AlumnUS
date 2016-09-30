ec3036d96ecf46f360d24fe6cee8e2d092228200
pranavmayuram <pranav.mayuram@gmail.com>
2016-09-30 00:55:05 -0400

    Add in formatting changes, and only wait until 5 failures to declare the job done

--- public/alumnus.js
+++ public/alumnus.js
@@ -1,18 +1,19 @@
 var alumnus = angular.module('alumnus', []);
 
 alumnus.controller('mainController', function ($scope, $http, $interval) {
+    $scope.excelUploadURL = window.location.href + "/api/uploadExcel";
     $scope.requestProcessing = {"total": null, "processed": null};
     var intervalFn;
     var finishCounter = 0;
 
     $scope.beginRequestProcessingCheck = function() {
         intervalFn = $interval(function() {
-            $http({method: "GET", url: "/api/requestProcessing"})
+            $http({method: "GET", url: window.location.href + "/api/requestProcessing"})
                 .success(function(result) {
                     console.log(result);
                     if (result["total"] == -1) {
                         ++finishCounter;
-                        if (finishCounter > 10) {
+                        if (finishCounter > 5) {
                             $scope.stopRequestProcessingCheck();
                             alert("Your job has been completed!");
                         }
@@ -39,4 +40,8 @@ alumnus.controller('mainController', function ($scope, $http, $interval) {
         // Make sure that the interval is destroyed too
         $scope.stopRequestProcessingCheck();
     });
+
+    $scope.loadingPercentage = function() {
+        return (($scope.requestProcessing.processed / $scope.requestProcessing.total)*100) + "%";
+    };
 });
